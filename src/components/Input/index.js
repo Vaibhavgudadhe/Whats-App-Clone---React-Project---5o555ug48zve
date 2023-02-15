27b8dc3837ebd83ac60ff618607706ea7a647a38
Vaibhavgudadhe <78964892+Vaibhavgudadhe@users.noreply.github.com>
2023-02-15 13:50:16 +0530

    Delete src directory

--- src/components/Input/index.js
+++
@@ -1,7 +0,0 @@
-import * as S from "./styles";
-
-const Input = ({ type, placeholder, ...rest }) => {
-  return <S.Input type={type} placeholder={placeholder} {...rest}></S.Input>;
-};
-
-export default Input;
